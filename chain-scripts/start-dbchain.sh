24aab07ab55a5f5c56b4b31446b22ed6c6ee14d4
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-07-25 17:47:15 +0200

    Merge dbchains repo

---
+++ chain-scripts/start-dbchain.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")/.."
+
+git submodule update --init
+
+cd blockscout-rs-neti/golem-base-tools/
+cargo build
+
+cd ../../golembase-op-geth
+patch -N <../chain-scripts/golembase-op-geth-enable-txpool-api.patch || true
+patch -N <../chain-scripts/golembase-op-geth-rpclorer-external-port.patch || true
+patch -N <../chain-scripts/golembase-op-geth-all-interfaces.patch || true
+docker compose up -d --build
+
+cd cmd/golembase
+go build -o golembase
+./golembase account create
+./golembase account fund
+
+echo
+echo "Running. Visit http://localhost:9000/ to verify."
+echo "RPC endpoint available at http://localhost:8545 and ws://localhost:8545"
